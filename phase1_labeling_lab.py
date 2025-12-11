fa2639dd1b87f2d129260efe342e11e3a7731e33
Dinzeyi2 <133053311+Dinzeyi2@users.noreply.github.com>
2025-12-10 23:01:11 -0500

    Update phase1_labeling_lab.py

--- phase1_labeling_lab.py
+++ phase1_labeling_lab.py
@@ -45,11 +45,9 @@ import webbrowser
 from collections import defaultdict, Counter
 import statistics
 
-from flask import Flask, render_template_string, request, jsonify, send_file
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import numpy as np
 from scipy import stats
-import pandas as pd
+
 
 try:
     from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, DateTime, Text, JSON
@@ -1167,3 +1165,4 @@ if __name__ == '__main__':
     
 
     app.run(debug=False, host='127.0.0.1', port=5001, threaded=True)
+
