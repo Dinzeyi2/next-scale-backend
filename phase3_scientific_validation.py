f82488ba92502dacec303a77ab83a625c2eafe90
Dinzeyi2 <133053311+Dinzeyi2@users.noreply.github.com>
2025-12-10 22:58:50 -0500

    Update phase3_scientific_validation.py

--- phase3_scientific_validation.py
+++ phase3_scientific_validation.py
@@ -50,8 +50,6 @@ from collections import defaultdict, Counter
 import pickle
 
 # Core imports
-from flask import Flask, render_template_string, request, jsonify, send_file
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import numpy as np
 from scipy import stats
 import pandas as pd
@@ -1454,3 +1452,4 @@ if __name__ == '__main__':
     
 
     app.run(debug=False, host='127.0.0.1', port=5003, threaded=True)
+
